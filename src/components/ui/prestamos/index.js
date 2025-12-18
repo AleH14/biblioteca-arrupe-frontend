9603187b94b6d9d37b0c75c972a5021255a30820
Alejandro Hernandez <pedrada914@gmail.com>
2025-12-18 11:00:59 -0600

    feat: Add index file to export all loan components from a single location

---
+++ src/components/ui/prestamos/index.js
@@ -0,0 +1,9 @@
+// Exportar todos los componentes de préstamos desde un solo archivo
+export { default as FilterPanel } from './FilterPanel';
+export { default as SearchSection } from './SearchSection';
+export { default as PrestamoCard } from './PrestamoCard';
+export { default as NuevoPrestamoModal } from './NuevoPrestamoModal';
+export { default as RenovarPrestamoModal } from './RenovarPrestamoModal';
+export { default as ConfirmarDevolucionModal } from './ConfirmarDevolucionModal';
+export { default as DetallesPrestamoModal } from './DetallesPrestamoModal';
+export { default as ConfirmarPrestamoModal } from './ConfirmarPrestamoModal';
